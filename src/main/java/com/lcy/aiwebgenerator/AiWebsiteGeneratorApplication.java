1769abd0c8c2bc2f9e45c3c7d757df37fcb7b638
chyuliang <cyliang@MacBook-Air.local>
2025-08-12 16:01:03 +0800

    Feat: add ai generate func

--- src/main/java/com/lcy/aiwebgenerator/AiWebsiteGeneratorApplication.java
+++ src/main/java/com/lcy/aiwebgenerator/AiWebsiteGeneratorApplication.java
@@ -1,11 +1,13 @@
 package com.lcy.aiwebgenerator;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(exposeProxy = true)
+@MapperScan("com.lcy.aiwebgenerator.mapper")
 public class AiWebsiteGeneratorApplication {
 
     public static void main(String[] args) {
