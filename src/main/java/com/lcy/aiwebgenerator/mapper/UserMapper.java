305bee55ecad2a7e69c8ea125b72c89f99cd1a54
chyuliang <cyliang@MacBook-Air.local>
2025-08-12 16:12:16 +0800

    Feat: add ai generate func #3

---
+++ src/main/java/com/lcy/aiwebgenerator/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.lcy.aiwebgenerator.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.lcy.aiwebgenerator.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author <a href="https://github.com/chyuliang"></a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
